d489b02c3f9bdb844d9b9ce1bf61190195212680
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-03-26 16:06:18 -0300

    feat: inicializando projeto

---
+++ src/app/index/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+import { styles } from "@/styles/styles-index";
+
+export default function Index(){
+    return (
+        <View style={styles.container} >
+            <Text style={styles.title} >hello world</Text>
+        </View>
+    )
+}
