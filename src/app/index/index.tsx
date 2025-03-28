abe075db1481b43986a4550f10034a709f8ac8a7
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-03-28 08:28:29 -0300

    feat: criação e estilização do header

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,10 +1,20 @@
-import { Text, View } from "react-native";
+import { Image, Text, Touchable, TouchableOpacity, View } from "react-native";
 import { styles } from "@/styles/styles-index";
+import { MaterialIcons } from "@expo/vector-icons";
+import { colors } from "@/styles/colors";
 
 export default function Index(){
     return (
         <View style={styles.container} >
-            <Text style={styles.title} >hello world</Text>
+            <View style={styles.header} >
+                <Image
+                    source={require("@/assets/logo.png")}
+                    style={styles.logo}
+                />
+                <TouchableOpacity>
+                    <MaterialIcons name="add" size={32} color={colors.green[300]} />
+                </TouchableOpacity>
+            </View>
         </View>
     )
 }
