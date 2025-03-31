78fefd873c84e02901c1fd904cb1042ca27f547e
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-03-31 14:51:06 -0300

    feat: implementando lista

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,4 +1,11 @@
-import { Image, Text, Touchable, TouchableOpacity, View } from "react-native";
+import {
+    FlatList,
+    Image,
+    Text,
+    Touchable,
+    TouchableOpacity,
+    View,
+} from "react-native";
 import { styles } from "@/styles/index/index";
 import { MaterialIcons } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
@@ -6,21 +13,36 @@ import { Categories } from "@/components/categories";
 import { Link } from "@/components/link";
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.header}>
-        <Image source={require("@/assets/logo.png")} style={styles.logo} />
-        <TouchableOpacity>
-          <MaterialIcons name="add" size={32} color={colors.green[300]} />
-        </TouchableOpacity>
-      </View>
-      <Categories />
-      <Link
-        name="Rockeseat"
-        url="https://www.rockeseat.som.br/"
-        onDetails={() => console.log("clicou")
-        }
-        />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <Image
+                    source={require("@/assets/logo.png")}
+                    style={styles.logo}
+                />
+                <TouchableOpacity>
+                    <MaterialIcons
+                        name="add"
+                        size={32}
+                        color={colors.green[300]}
+                    />
+                </TouchableOpacity>
+            </View>
+            <Categories />
+            <FlatList
+                data={["1", "2", "3"]}
+                keyExtractor={(item) => item}
+                renderItem={({ item }) => (
+                    <Link
+                        name="Rocketseat"
+                        url="https://www.rocketseat.com.br/"
+                        onDetails={() => console.log("Clicou!")}
+                    />
+                )}
+                style={styles.links}
+                contentContainerStyle={styles.linksContent}
+                showsVerticalScrollIndicator={false}
+            />
+        </View>
+    );
 }
