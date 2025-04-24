fab8bdd50a67b4cb76a4d9ded7674246b6470cc8
felipeisraelsantos <felipe.israel@aluno.ifsp.edu.br>
2025-04-24 08:57:56 -0300

    feat: Filtrando Links - implementado um filtro por categoria como uma dependência, exibindo links de acordo com a categoria selecionada.

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -25,8 +25,12 @@ export default function Index() {
     async function getLinks() {
         try {
             const response = await linkStorage.get();
-            setLinks(response);
-            console.log(response);
+
+            const filtered = response.filter(
+                (link) => link.category === category
+            );
+
+            setLinks(filtered);
         } catch (error) {
             Alert.alert("Erro", "Não foi possível listar os links");
         }
@@ -34,9 +38,9 @@ export default function Index() {
 
     useFocusEffect(
         useCallback(() => {
-          getLinks()
-        }, [])
-      ) 
+            getLinks();
+        }, [category])
+    );
 
     return (
         <View style={styles.container}>
